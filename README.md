37d688615a8936269953b26822cfe34d191f306c
amanda_gibson <gibson.amanda.mae@gmail.com>
2019-02-03 14:49:21 +0100

    reformats code

--- README.md
+++ README.md
@@ -1,5 +1,6 @@
-BMI Calculator
+# BMI Calculator
 
 The BMI Calculator takes an input of weight and height and returns a body mass percentage and class; Underweight, Normal, Overweight, or Obese.
 
+*********
 Tools used in this project: JavaScript
--- features/index.feature.js
+++ features/index.feature.js
@@ -7,20 +7,20 @@ describe('User can calculate BMI', () => {
         await browser.init()
         await browser.visitPage('http://localhost:8080/')
     });
-
+    
     beforeEach(async () => {
         await browser.page.reload();
     });
-
+    
     after(async () => {
         await browser.close();
     });
-
+    
     it('by inputing his weight and height', async () => {
-
+        
         await browser.fillIn("input[id='weight-in-kgs']", { with: "95" })
         await browser.fillIn("input[id='height-in-cm']", { with: "185" })
-
+        
         await browser.clickOnButton("button")
         let content = await browser.getContent("span[id='display_value']")
         expect(content).to.eql('Your BMI is 27.76');
